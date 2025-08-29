17badc8a992dbdecc79e1209ce3e7ee6397e8c9e
M1ngsan636 <suhardiman@airasia.com>
2025-08-29 14:16:02 +0700

    Fix Vercel routing and test page

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php
-phpinfo();
+// test.php
+header('Content-Type: text/html');
+echo "<h1>PHP is working on Vercel!</h1>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+echo "<a href='/'>Go to main app</a>";
 ?>
